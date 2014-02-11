f958f12219ebc1986158a00ce1ed18907e2fb7f1
Robert Brandin <robert.brandin@factorylabs.com>
2014-02-11 09:31:29 -0700

    Add notes for TokenGenerator and Redirect classes

--- app/models/redirect.rb
+++ app/models/redirect.rb
@@ -5,6 +5,9 @@ class Redirect < ActiveRecord::Base
 
   before_create :assign_url_token
 
+  # Public: Mimics the now-deprecated find_by_x functionality of Rails, but returns
+  # a null Redirect if the redirect does not exist.
+
   def self.find_by_url_token(url_token)
     self.where(url_token: url_token).last || NullRedirect.new
   end
--- lib/token_generator.rb
+++ lib/token_generator.rb
@@ -1,5 +1,11 @@
 class TokenGenerator
 
+  # Public: Generates a random, unique token for the Redirect model.
+
+  # This method was cribbed from StackOverflow (http://stackoverflow.com/a/12109098/560632).
+
+  # TODO: Tests.
+
   def self.generate
     loop do
       token = SecureRandom.urlsafe_base64(6)
